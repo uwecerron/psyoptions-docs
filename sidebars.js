d4d740b9358e18edb9a94f77d71c75a9fcaeccab
Taylor Johnson <cjohns28@terpmail.umd.edu>
2021-04-11 22:57:30 +0800

    add devnet token/faucet set up instructions (#10)

--- sidebars.js
+++ sidebars.js
@@ -2,12 +2,13 @@ module.exports = {
   someSidebar: {
     "Getting Started": ["intro", "glossary"],
     "Program Integration": ["program-integration-start"],
-    "Clients": [
+    Clients: [
       "client-integration-overview",
       "javascript-functions",
       {
         Rust: ["rust-start"],
       },
     ],
+    "Devnet Testing": ["devnet-tokens"],
   },
 };
